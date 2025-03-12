ac29df7e43b47176b4ef9e1fdb45a13a26b275d8
iliasoafathi <iliasafathi14@gmail.com>
2025-03-12 13:19:19 +0000

    init project
    c

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white text-center p-4">
+    <h1 className="text-3xl font-bold">Bienvenue sur Tailwind CSS</h1>
+    <p className="mt-2">Ceci est un projet React avec Tailwind CSS.</p>
+  </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Indique que Tailwind doit analyser ces fichiers
+  ],
+  theme: {
+    extend: {}, // Personnalisation ici si nécessaire
+  },
+  plugins: [],
+};
